0f2a914acf34ec2ec737b1d6d6483a06d6f1d03e
Kira <k.liatambur@ukma.edu.ua>
2026-03-08 02:39:44 +0200

    add lambdas to search

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -20,9 +20,9 @@ public class Search {
                 case 1:
                     String findBySNM = checkInput.checkString("Введіть ПІБ для пошуку: ", "Ви не ввели ПІБ для пошуку.");
                     List<Student> results = findBySNM(findBySNM);
-                    if (results.isEmpty()) {
+                    if (results.isEmpty())
                         System.out.println("Студентів з таким ПІБ не знайдено");
-                    } else {
+                    else {
                         System.out.println("Знайдено студентів: " + results.size());
                         for (Student s : results) {
                             System.out.println("- " + s);
@@ -32,11 +32,11 @@ public class Search {
                     break;
                 case 2:
                     int findByYear = checkInput.checkInt("Введіть курс для пошуку: ", "Ви ввели курс неправильно.");
-
                     List<Student> results1 = findByYear(findByYear);
-                    if (results1.isEmpty()) {
+
+                    if (results1.isEmpty())
                         System.out.println("Студентів такого курсу не знайдено");
-                    } else {
+                    else {
                         System.out.println("Знайдено студентів: " + results1.size());
                         for (Student s : results1) {
                             System.out.println("- " + s);
@@ -46,7 +46,8 @@ public class Search {
                     break;
                 case 3:
                     int findByGroup = checkInput.checkInt("Введіть групу для пошуку: ", "Ви ввели групу неправильно.");
-                    List<Student> results2 = findByGroup(findByGroup);
+                    List <Student> results2 = findByGroup(findByGroup);
+
                     if (results2.isEmpty()) {
                         System.out.println("Студентів такої групи не знайдено");
                     } else {
@@ -68,11 +69,10 @@ public class Search {
             case 1:
                 System.out.println("Введіть ПІБ для пошуку: ");
                 String findBySNMt = checkInput.checkString("Введіть ПІБ для пошуку: ", "Ви не ввели ПІБ для пошуку.");
-
                 List<Teacher> results = findBySNMt(findBySNMt);
-                if (results.isEmpty()) {
+
+                if (results.isEmpty())
                     System.out.println("Викладачів з таким ПІБ не знайдено");
-                }
                 else {
                     System.out.println("Знайдено викладачів: " + results.size());
                     for (Teacher t : results) {
@@ -86,55 +86,29 @@ public class Search {
     }
 
     public List<Teacher> findBySNMt(String snm) {
-        List<Teacher> foundTeachers = new ArrayList<>();
-        if (snm == null || snm.isEmpty()) return foundTeachers;
-
-        for (Teacher teacher :allObjects.allTeachers()) {
-            if (teacher != null) {
-                String SNM = teacher.getPersonSurname() + " " + teacher.getPersonName() + " " + teacher.getMiddleName();
-                if (snm.equalsIgnoreCase(SNM)) {
-                    foundTeachers.add(teacher);
-                }
-            }
-        }
-        return foundTeachers;
+        return allObjects.allTeachers().stream().filter(teacher -> {
+                    String SNM = teacher.getPersonSurname() + " " + teacher.getPersonSurname() + " " + teacher.getMiddleName();
+                    return snm.equalsIgnoreCase(SNM);
+        } )
+        .toList();
     }
 
     public List<Student> findBySNM(String snm) {
-        List<Student> foundStudents = new ArrayList<>();
-        if (snm == null || snm.isEmpty()) return foundStudents;
-
-        for (Student student :allObjects.allStudents()) {
-            if (student != null) {
-                String SNM = student.getPersonSurname() + " " + student.getPersonName() + " " + student.getMiddleName();
-                if (snm.equalsIgnoreCase(SNM)) {
-                    foundStudents.add(student);
-                }
-            }
-        }
-        return foundStudents;
+        return allObjects.allStudents().stream().filter(student -> {
+                    String SNM = student.getPersonSurname() + " " + student.getPersonName() + " " + student.getMiddleName();
+                    return snm.equalsIgnoreCase(SNM);
+        })
+        .toList();
     }
 
     public List<Student> findByYear(int year) {
-        List<Student> foundStudents = new ArrayList<>();
-
-        for (Student student :allObjects.allStudents()) {
-            if (student != null && year == student.getCourseNumber() ) {
-                foundStudents.add(student);
-            }
-        }
-        return foundStudents;
+        return allObjects.allStudents().stream().filter(student -> student.courseNumber == year)
+        .toList();
     }
 
     public List<Student> findByGroup(int group) {
-        List<Student> foundStudents = new ArrayList<>();
-
-        for (Student student :allObjects.allStudents()) {
-            if (student != null && group == student.getGroupNumber() ) {
-                foundStudents.add(student);
-            }
-        }
-        return foundStudents;
+        return allObjects.allStudents().stream().filter(student -> student.groupNumber == group)
+        .toList();
     }
 
 
